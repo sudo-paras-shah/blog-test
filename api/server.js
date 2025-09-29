17761f4a9feade9a00bc27c3e682edc99bafdb73
Paras Shah <paras178a@gmail.com>
2025-09-29 20:20:09 +0530

    Add simple login and logout functionality using token
    Add env to gitignore

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
+import 'dotenv/config';
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +29,7 @@ app.use((req, res, next) => {
 	next();
 });
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -27,6 +37,44 @@ app.get('/', (req, res) => {
 
 app.get('/blog', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/blog.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/login.html'));
+});
+
+app.get('/api/posts', async (req, res) => {
+    const { data, error } = await supabase
+        .from('posts')
+        .select('*')
+        .order('created_at', { ascending: false });
+    if (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
+    res.json(data);
+});
+
+app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body;
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    
+    if (error) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    
+    res.cookie('auth_token', data.session.access_token, {
+        httpOnly: true,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        // secure: true,
+        // sameSite: true
+    });
+    res.redirect('/blog');
+});
+
+app.get('/api/logout', (req, res) => {
+    res.clearCookie('auth_token');
+    res.redirect('/blog'); 
 });
 
 app.listen(PORT, () => {
